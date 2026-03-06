5c7941f30a08ae3562b0e9469411e8f50ca3c0eb
Mujeeb Ullah Khan <kmehrankhan757@gmail.com>
2026-03-06 16:45:19 +0700

    Add database configuration to config.php

---
+++ MINI ERP/config.php
@@ -0,0 +1,6 @@
+<?php
+$servername = getenv('DB_HOST') ?: "localhost";
+$username   = getenv('DB_USER') ?: "root";
+$password   = getenv('DB_PASSWORD') ?: "";
+$dbname     = getenv('DB_NAME') ?: "erp_local";
+?>
